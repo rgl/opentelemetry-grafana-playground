5c9faa0160ce6a1f589193b52df44dc23ef9666f
Rui Lopes <rgl@ruilopes.com>
2024-02-15 10:24:18 +0000

    upgrade dependencies

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a [OpenTelemetry](https://github.com/open-telemetry/opentelemetry-dotnet)/[Grafana](https://github.com/grafana/grafana)/[Tempo](https://github.com/grafana/tempo) playground.
 
-# Usage (Ubuntu 20.04)
+# Usage (Ubuntu 22.04)
 
 ```bash
 # create the environment defined in docker-compose.yml
@@ -21,7 +21,7 @@ docker compose down --remove-orphans
 
 # Notes
 
-* .NET uses the [Activity class](https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.activity?view=net-6.0) to encapsulate the [W3C Trace Context](https://www.w3.org/TR/trace-context/).
+* .NET uses the [Activity class](https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.activity?view=net-8.0) to encapsulate the [W3C Trace Context](https://www.w3.org/TR/trace-context/).
   * The Activity `Id` property contains to the [W3C `traceparent` header value](https://www.w3.org/TR/trace-context/#traceparent-header).
     * It looks alike `00-98d483b6d0e3a6d012b11e23737faa50-6ac18089ab13c12e-01`.
     * It has four fields: `version`, `trace-id`, `parent-id` (aka `span-id`), and `trace-flags`.
--- quotes/Program.cs
+++ quotes/Program.cs
@@ -2,33 +2,28 @@ using OpenTelemetry.Exporter;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 
-// Allow HTTP/2 over TCP (to be able to use insecure gRPC to communicate with Tempo).
-// NB this is required to connect to our test Tempo instance.
-// NB you should not do this in production.
-// See https://docs.microsoft.com/en-us/aspnet/core/grpc/troubleshoot?view=aspnetcore-6.0#call-insecure-grpc-services-with-net-core-client
-AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddOpenTelemetryTracing(tpb => tpb
-    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Configuration.GetValue<string>("Otlp:ServiceName")))
-    .AddOtlpExporter(oeo =>
-        {
-            // NB This overrides the OTEL_EXPORTER_OTLP_ENDPOINT environment variable.
-            oeo.Endpoint = new Uri(builder.Configuration.GetValue<string>("Otlp:Endpoint"));
-            // NB This overrides the OTEL_EXPORTER_OTLP_PROTOCOL environment variable.
-            oeo.Protocol = OtlpExportProtocol.Grpc;
-            // NB OtlpExportProtocol.HttpProtobuf does not seem to be supported by Tempo.
-            // see https://github.com/grafana/tempo/discussions/1172
-            //oeo.Protocol = OtlpExportProtocol.HttpProtobuf;
-        }
-    )
-    .AddConsoleExporter()
-    .AddAspNetCoreInstrumentation()
-);
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(builder.Configuration.GetValue<string>("Otlp:ServiceName")!))
+    .WithTracing(tracing => tracing
+        .AddAspNetCoreInstrumentation()
+        .AddOtlpExporter(oeo =>
+            {
+                // NB This overrides the OTEL_EXPORTER_OTLP_ENDPOINT environment variable.
+                oeo.Endpoint = new Uri(builder.Configuration.GetValue<string>("Otlp:Endpoint")!);
+                // NB This overrides the OTEL_EXPORTER_OTLP_PROTOCOL environment variable.
+                oeo.Protocol = OtlpExportProtocol.Grpc;
+                // NB OtlpExportProtocol.HttpProtobuf does not seem to be supported by Tempo.
+                // see https://github.com/grafana/tempo/discussions/1172
+                //oeo.Protocol = OtlpExportProtocol.HttpProtobuf;
+            }
+        )
+        .AddConsoleExporter()
+    );
 
 builder.Logging.Configure(options =>
     {
